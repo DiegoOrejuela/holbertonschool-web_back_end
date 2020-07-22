9cfe2df42620201ecab3c996d922b5c50cbb5401
Diego Orejuela <dfrodriguezor@gmail.com>
2020-07-22 16:15:27 +0000

    master 0x00-python_variable_annotations #comment 0. Basic annotations add documentation function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,9 @@
 
 
 def add(a: float, b: float) -> float:
+    '''
+    Type-annotated function add that takes
+    a float a and a float b as arguments and
+    returns their sum as a float.
+    '''
     return a + b
