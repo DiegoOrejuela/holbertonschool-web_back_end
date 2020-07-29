c776e7fdbf973aa71fdee388b66be5c6659a2040
Diego Orejuela <dfrodriguezor@gmail.com>
2020-07-29 20:27:52 +0000

    master 0x04. Pagination #comment 0. Simple helper function fixed typed notation

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -5,7 +5,7 @@
 """
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
     """
     Function should return a tuple of size two containing a start
     index and an end index corresponding to the range of indexes to
