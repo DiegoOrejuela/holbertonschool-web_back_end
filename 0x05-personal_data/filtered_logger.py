fc722eb18d22378c2e155f23b83e8bd86cfc3eaf
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-02 21:58:51 +0000

    master 0x05. Personal data #comment 1. Log formatter add notation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -3,9 +3,11 @@
 0. Regex-ing
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str):
     '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(r"{}=(.*?){}".format(field, separator),
