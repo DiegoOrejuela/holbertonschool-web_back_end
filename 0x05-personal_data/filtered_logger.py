98cfec745347c9b7d918666da9efc297a045998f
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-02 22:08:18 +0000

    master 0x05. Personal data #comment 1. Log formatter fix return annotation

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -7,8 +7,8 @@ from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str):
-    '''returns the log message obfuscated'''
+                 separator: str) -> str:
+    """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(r"{}=(.*?){}".format(field, separator),
                          f'{field}={redaction}{separator}', message)
