10f3d7ec76277aa977104fb544ea8953042e2c76
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-03 20:37:39 +0000

    master 0x05. Personal data #comment 5. Encrypting passwords

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+"""
+5. Encrypting passwords
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ function that expects one string argument and returns
+    a salted, hashed password, which is a byte string"""
+    binary_password = bytes(password, "ascii")
+    return bcrypt.hashpw(binary_password, bcrypt.gensalt())
