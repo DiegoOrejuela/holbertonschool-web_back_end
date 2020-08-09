b566c847ef3fbfdc02fecec4b3935b65282f6de5
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-09 22:36:24 +0000

    master 0x06. Basic authentication #comment 2. Error handler: Forbidden

--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> None:
       This endpoint must raise a 401 error by using abort
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    Return:
+      This endpoint must raise a 403 error by using abort
+    """
+    abort(403)
