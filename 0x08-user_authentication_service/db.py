05eeb78f8ce3452a22bfcc5d0bfcfe46a9855580
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-17 06:14:00 +0000

    master 0x08. User authentication service #comment 2. Find user anotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs: dict):
+    def find_user_by(self, **kwargs: dict) -> User:
         """ 2. Find user by
         """
         results = self._session.query(User).filter_by(**kwargs)
