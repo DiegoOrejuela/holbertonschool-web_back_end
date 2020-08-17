4f8955afe5686b86a8c5b33b78cdaa6203675291
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-17 23:13:13 +0000

    master 0x08. User authentication service #comment 7. Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,12 +3,34 @@
 App module
 """
 from flask import Flask, escape, request, jsonify
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def welcome() -> str:
+    """ Welcome
+    """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """ Register user
+    """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": "{}".format(user.email),
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
