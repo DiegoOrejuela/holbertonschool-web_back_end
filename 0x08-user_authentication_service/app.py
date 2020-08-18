971bfaab4343954188585f04c8f3916a64908e9c
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-18 02:23:02 +0000

    master 0x08. User authentication service #comment 19. Update password end-point

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -93,5 +93,22 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route('/update_password', methods=['PUT'])
+def update_password() -> str:
+    """ If the email is not registered, respond with a 403 status code.
+        Otherwise, generate a token and respond with a 200 HTTP status
+    """
+
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
