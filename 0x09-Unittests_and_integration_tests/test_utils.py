cfd73a4c35f87fa38bd2d0bd3bbb226d04c5e367
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-23 21:31:58 +0000

    master 0x09. Unittests and Integration Tests #comment 1. Parameterize a unit test

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,7 @@ import unittest
 from utils import access_nested_map
 from parameterized import parameterized, param
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """ Test Access Nested Map
     """
@@ -14,6 +15,15 @@ class TestAccessNestedMap(unittest.TestCase):
         param({"b": 2}, nested_map={"a": {"b": 2}}, path=("a",)),
         param(2, nested_map={"a": {"b": 2}}, path=("a", "b"))
     ])
-    def test_access_nested_map(self, expected, nested_map, path): 
+    def test_access_nested_map(self, expected, nested_map, path):
         """ Test utils.access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        param(KeyError, nested_map={}, path=("a",)),
+        param(KeyError, nested_map={"a": 1}, path=("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, expected, nested_map, path):
+        """ Test utils.access_nested_map with exception"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
