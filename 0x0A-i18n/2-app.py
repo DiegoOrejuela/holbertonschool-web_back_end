85d066a6fe186db2cf35e6524aa89611ebf269e4
Diego Orejuela <dfrodriguezor@gmail.com>
2020-08-28 03:19:03 +0000

    master #comment 2. Get locale from request fix documenation get_locate

--- 0x0A-i18n/2-app.py
+++ 0x0A-i18n/2-app.py
@@ -4,6 +4,7 @@ App module
 """
 from flask import Flask, request, render_template
 from flask_babel import Babel
+from typing import Union
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -21,7 +22,9 @@ app.config.from_object('1-app.Config')
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> Union[str, None]:
+    """ get locale
+    """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
