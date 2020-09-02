48075654f676768b6bc67e95265f712ef936100e
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-02 00:27:15 +0000

    master 0x0B. Redis basic #comment 0. Writing strings to Redis

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+Redis basic module
+'''
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+    """ Cache class """
+
+    def __init__(self):
+        """ __init __ """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Takes a data argument and returns a string. The method
+        should generate a random key (e.g. using uuid), store the input
+        data in Redis using the random key and return the key.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
