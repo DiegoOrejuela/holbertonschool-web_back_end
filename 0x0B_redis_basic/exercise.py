23981a44eee692e8985b9f563e2e7eae9c05e3db
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-02 04:23:26 +0000

    master 0x0B. Redis basic #comment 3. Storing lists notation

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -27,7 +27,7 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
-def call_history(method: callable) -> Callable:
+def call_history(method: Callable) -> Callable:
     """
     Decorator to store the history of inputs and outputs
     for a particular function.
