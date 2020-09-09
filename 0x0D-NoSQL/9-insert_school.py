25512a01c8cef357f734a99223f9f434d4cf56f8
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-09 03:04:19 +0000

    master 0. List all databases #comment 9. Insert a document in Python

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" insert school module
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """ Python function that inserts a new document in a
+        collection based on kwargs
+    """
+    return mongo_collection.insert_one(kwargs)
