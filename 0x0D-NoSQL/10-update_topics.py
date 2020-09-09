fb77009de531bc0e5a91fe1726b1752fb23e3e8e
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-09 03:23:30 +0000

    master 0. List all databases #comment 10. Change school topics change method to update_many

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -7,7 +7,7 @@ def update_topics(mongo_collection, name, topics):
     """ Python function that changes all topics of a
         school document based on the name
     """
-    return mongo_collection.update_one(
+    return mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
     )
