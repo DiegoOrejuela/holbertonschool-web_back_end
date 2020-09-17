a20cc6f918cf91b5ade14d59d0245aff60e9bfa6
DiegoOrejuela <34580380+DiegoOrejuela@users.noreply.github.com>
2020-09-16 22:02:23 -0500

    Create 1-promise.js

---
+++ 0x0F-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default getFullResponseFromAPI(success) {
+    return new Promise(function(resolve, reject){
+        if (success == true) {
+            resolve({ status: 200, body: 'Success' })
+        } else if ( success == false ) {
+            reject('The fake API is not working currently')
+        }
+    })
+}
