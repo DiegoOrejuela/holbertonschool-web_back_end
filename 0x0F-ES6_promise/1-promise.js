69d18ab3c49b2265a8d01148da8bbd3f8018851c
DiegoOrejuela <34580380+DiegoOrejuela@users.noreply.github.com>
2020-09-16 22:09:30 -0500

    Update 1-promise.js

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success == true) {
       resolve({ status: 200, body: 'Success' });
     } else if ( success == false ) {
-      reject('The fake API is not working currently');
+      reject(Error('The fake API is not working currently'));
     }
   });
 }
