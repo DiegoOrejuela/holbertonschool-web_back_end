f227c5ebdc60ab86fff1f5defb4b9c066539640e
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-24 04:40:22 +0000

    master 0x11. ES6 data manipulation #comment 8. Clean set fix refactor

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
-export default function cleanSet(set, startString) {
-  if (!startString || !startString.length) return '';
-  return Array.from(set).map(
-    (element) => (element.startsWith(startString) ? element.substring(startString.length) : ''),
-  ).filter(Boolean).join('-');
+export default function cleanSet(set, str) {
+  if (!str || !str.length) return '';
+  let val = '';
+  for (const el of set) {
+    if (el && el.startsWith(str)) {
+      val += val.length === 0 ? el.replace(str, '') : el.replace(str, '-');
+    }
+  }
+  return val;
 }
+
