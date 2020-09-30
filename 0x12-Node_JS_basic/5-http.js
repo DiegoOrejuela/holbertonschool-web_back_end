f17523c1b04caf2facbb6145d5384a00c201650f
Diego Orejuela <dfrodriguezor@gmail.com>
2020-09-30 03:25:58 +0000

    master 0x12. NodeJS Basics #comment 5. Create a more complex HTTP server using Node's HTTP module define function in file

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,5 +1,56 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise(((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, paramsStudents) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      let students = paramsStudents;
+      students = students.split('\n');
+      const headers = students.shift().split(',');
+
+      const groupingStudentsField = {};
+      const studentsObjects = [];
+
+      students.forEach((student) => {
+        if (student) {
+          const studentInfo = student.split(',');
+          const studentObject = {};
+
+          headers.forEach((header, index) => {
+            studentObject[header] = studentInfo[index];
+            if (header === 'field') {
+              if (groupingStudentsField[studentInfo[index]]) {
+                groupingStudentsField[studentInfo[index]].push(studentObject.firstname);
+              } else {
+                groupingStudentsField[studentInfo[index]] = [studentObject.firstname];
+              }
+            }
+          });
+          studentsObjects.push(studentObject);
+        }
+      });
+      const numberStudents = `Number of students: ${studentsObjects.length}`;
+
+      let response = `${numberStudents}\n`;
+      console.log(numberStudents);
+
+      for (const groupStudent in groupingStudentsField) {
+        if (groupingStudentsField[groupStudent]) {
+          const listStudents = groupingStudentsField[groupStudent];
+          const responseGroupStudents = `Number of students in ${groupStudent}: ${listStudents.length}. List: ${listStudents.join(', ')}`;
+          response += `${responseGroupStudents}\n`;
+          console.log(responseGroupStudents);
+        }
+      }
+      resolve(response.substring(0, response.length - 1));
+    });
+  }));
+}
 
 const hostname = '127.0.0.1';
 const port = 1245;
