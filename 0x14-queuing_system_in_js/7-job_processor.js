4efe39325757d80cb7826837e8fe32ce8b87e95b
Diego Orejuela <dfrodriguezor@gmail.com>
2020-10-13 05:10:53 +0000

    master 0x14. Queuing System in JS #comment 9. Track progress and errors with Kue: Create the Job processor

---
+++ 0x14-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,13 @@
+import kue from 'kue';
+const queue = kue.createQueue();
+
+const blacklistedPhoneNumbers = ['4153518780', '4153518781'];
+
+function sendNotification (phoneNumber, message, job, done) {
+    if (blacklistedPhoneNumbers.includes(phoneNumber)) throw Error(`Phone number ${phoneNumber} is blacklisted`)
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process('push_notification_code_2', 2, function(job, done){
+    sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
